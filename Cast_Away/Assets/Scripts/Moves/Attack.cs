34b9f4c99b6d593854860927c622fb1856832904
bowenlin1101 <bowenblinblue@gmail.com>
2024-03-27 19:24:58 -0400

    added attacks and added more descriptions to moves

---
+++ Cast_Away/Assets/Scripts/Moves/Attack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack
+{
+    // Start is called before the first frame update
+    public string AttackName {get; set;}
+    public float Damage {get; set;}
+    public string Type {get; set;}
+    public float Speed {get; set;}
+    public int NumberOfAttacks {get; set;}
+    public float Interval {get; set;}
+    public Sprite Sprite {get; set;}
+    public Attack(string attackName, float damage, string type, float speed, int numberOfAttacks, float Interval, Sprite sprite) {
+        this.MoveName = attackName;
+        this.Damage = damage;
+        this.Type = type;
+        this.Speed = speed;
+        this.NumberOfAttacks = numberOfAttacks;
+        this.Interval = Interval;
+        this.Sprite = sprite;
+    }
+}
