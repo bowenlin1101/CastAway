9f313657d7035695956c219a3aa7b1cb1226dff3
bowenlin1101 <bowenblinblue@gmail.com>
2024-03-30 13:55:01 -0400

    added new paramter to interval

--- Cast_Away/Assets/Scripts/Aliens/SuperiorAlienScript.cs
+++ Cast_Away/Assets/Scripts/Aliens/SuperiorAlienScript.cs
@@ -15,8 +15,8 @@ public class SuperiorAlienScript : BaseAlienScript
         this.sprite = Resources.Load<Sprite>("AlienSprites/Comic Battle Royale/2D Character - Alien/Variant C/Sprites/Character/walk/side/01");
         this.attacks = new List<EnemyAttack>();
         this.acts = new List<Move>();
-        attacks.Add(new EnemyAttack("Hammer Spin", 100, "Physical", 20f, 3, (1f,1f), "Hammer", 1));
-        attacks.Add(new EnemyAttack("Acid Rain", 125, "Chemical", 30f, 10, (0.5f,1f), "Green ball", 2));
+        attacks.Add(new EnemyAttack("Hammer Spin", 100, "Physical", 20f, 3, (1f,1f, 1), "Hammer", 1));
+        attacks.Add(new EnemyAttack("Acid Rain", 125, "Chemical", 30f, 10, (0.5f,1f, 1), "Green ball", 2));
         acts.Add(new Move("Plead", 10, "Emotional", "", "", ""));
         acts.Add(new Move("Appeal", 10, "Emotional", "", "", ""));
         acts.Add(new Move("Flex Muscles", 10, "Emotional", "", "", ""));
--- Cast_Away/Assets/Scripts/Moves/EnemyAttack.cs
+++ Cast_Away/Assets/Scripts/Moves/EnemyAttack.cs
@@ -6,10 +6,10 @@ public class EnemyAttack : Attack
 {
     public float Speed {get; set;}
     public int NumberOfAttacks {get; set;}
-    public (float, float) Interval {get; set;}
+    public (float, float, int) Interval {get; set;}
     public string Sprite {get; set;}
     public int AttackPattern {get; set;}
-    public EnemyAttack(string attackName, float damage, string type, float speed, int numberOfAttacks, (float, float) Interval, string sprite, int attackPattern) : base(attackName, damage, type) {
+    public EnemyAttack(string attackName, float damage, string type, float speed, int numberOfAttacks, (float, float, int) Interval, string sprite, int attackPattern) : base(attackName, damage, type) {
         this.Speed = speed;
         this.NumberOfAttacks = numberOfAttacks;
         this.Interval = Interval;
